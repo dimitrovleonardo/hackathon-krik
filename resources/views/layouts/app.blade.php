b3ecc9958fdf5c1516e1294a270d1cb8bd8d9fa8
Leonardo Dimitrov <leonardo.dimitrov@accesco.co>
2024-09-20 16:53:47 +0200

    init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<x-guest-layout>
+    <section class="bg-cover bg-[url('http://127.0.0.1:8000/images/Homepage/herosection_1920x1120px.jpg')] h-screen font-montserrat">
+        <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 flex flex-col items-center">
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl uppercase w-[55%]">ПРОМЕНАТА ДОАЃА ОД МЛАДИТЕ</h1>
+
+                <h2 class="mb-4 mt-20 text-lg font-bold tracking-tight leading-none text-black lg:text-3xl">Кои Сме Ние?</h2>
+                <p class="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 lg:px-48  w-[61%]">Центарот за младински активизам Крик е невладина, непрофитна организација основана од млади луѓе, предводена од млади луѓе и работи за и со млади луѓе.</p>
+                <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 w-[30%]">
+                    <a href="#" class="w-full py-3 px-5 sm:ms-4 text-[24px] font-medium text-gray-900 focus:outline-none bg-[#b8afda] rounded-3xl border border-gray-200 hover:bg-[#9e96be]">
+                        Повеќе За Нас
+                    </a>
+                </div>
+            </div>
+    </section>
+
+</x-guest-layout>
