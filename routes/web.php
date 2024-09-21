879ecdae32e07c472b5924caad69621701f863ec
Dimitar <dimitarkraljevski@gmail.com>
2024-09-21 16:06:35 +0200

    added services/ourTeam/Donation

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,25 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+Route::get('/ourTeam', function () {
+    return view('ourTeam');
+})->name('ourTeam');
+Route::get('/donation', function () {
+    return view('donation');
+})->name('donation');
+Route::get('/teamMember1', function () {
+    return view('team.member1');
+})->name('member1');
+Route::get('/teamMember2', function () {
+    return view('team.member2');
+})->name('member2');
+Route::get('/teamMember3', function () {
+    return view('team.member3');
+})->name('member3');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
