03c08a01924faba517a5c12a1f42643b38a91864
Leonardo Dimitrov <leonardo.dimitrov@accesco.co>
2024-09-21 13:08:40 +0200

    homepage, footer, about-us finished

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,134 @@
+<x-guest-layout>
+    <section>
+        <div
+            class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+            <img class="w-full rounded-2xl mix-blend-luminosity" src="{{asset('images/about_us.png')}}" alt="students">
+            <div class="mt-4 md:mt-0">
+                <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-gray-900">Центарот за
+                    младинскиот активизам Крик е невладина, непрофитна организација основана од страна на млади луѓе,
+                    предводена од млади луѓе и работи за и со младите луѓе. </h2>
+                <p class="mb-6 font-light text-gray-500 md:text-lg">Целната група со која КРИК работи
+                    се сите луѓе (но главно млади) кои сакаат да придонесат и направат промени во полето на екологија,
+                    критичко размислување и комуникациски вештини, како и рабење за благостојба на социјално исклучените
+                    групи од општеството и маргинализираните групи. Крик се стреми кон поттикнување на поголема
+                    партиципација на младите, младински активизам, да ги охрабри младите луѓе да бидат повеќе вклучени
+                    во процесот на креирање политики на локално и на национално ниво.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb-12">
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 py-4 lg:px-6">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">За Крик</h2>
+        </div>
+        <hr class="h-1 w-full bg-black">
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 py-8 lg:px-6">
+            <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-[#f8ac2e]">Мисија</h2>
+            <p>Да понуди активности за зајакнување на капацитетите на младите и лицата со попреченост преку развој на
+                младинската работа и обезбедување услуги за социјална заштита.</p>
+        </div>
+        <hr class="h-1 w-full bg-black">
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 py-8 lg:px-6">
+            <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-[#f8ac2e]">Визија</h2>
+            <p>Да создаде инклузивно општество каде младите и лицата со попреченост се подеднакво вклучени и имаат
+                целосно развиени капацитети активно да придонесуваат за заедницата.</p>
+        </div>
+        <hr class="h-1 w-full bg-black">
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 py-8 lg:px-6">
+            <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-[#f8ac2e]">Нашите Вредности</h2>
+            <p>Развивање на младинската работа, поттикнување младински активизам и волонтеризам. Поголема вклученост на
+                младите исклучени од општеството преку учество на работилници, настани и обуки.
+                Развој на услуги за социјална заштита, сместување и грижа за лица со интелектуална и/или телесна
+                попреченост и други услови. Поттикнување на социјалната вклученост на маргинализираните групи млади и
+                возрасни во сите сфери на општественото делување.</p>
+        </div>
+        <hr class="h-1 w-full bg-black">
+    </section>
+    <section class="mb-12">
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 py-4 lg:px-6">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Најчесто поставувани прашања</h2>
+        </div>
+        <div class="gap-8 items-center px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-1 py-8 lg:px-6">
+            <div class="mb-4">
+                <div
+                    class="flex flex-col justify-between bg-[#B8AFDA] p-8 w-full rounded-3xl text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
+                    <div class="flex justify-between items-center w-full">
+                        <span class="text-2xl mb-3">01. Lorem ipsum</span>
+                        <span class="h-6 w-6 flex items-center justify-center text-teal-500 ml-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="51" viewBox="0 0 50 51" fill="none">
+<path d="M25 50.1992C18.3696 50.1992 12.0107 47.5653 7.32233 42.8769C2.63392 38.1885 0 31.8296 0 25.1992C0 18.5688 2.63392 12.21 7.32233 7.52155C12.0107 2.83314 18.3696 0.199219 25 0.199219C31.6304 0.199219 37.9893 2.83314 42.6777 7.52155C47.3661 12.21 50 18.5688 50 25.1992C50 31.8296 47.3661 38.1885 42.6777 42.8769C37.9893 47.5653 31.6304 50.1992 25 50.1992ZM37.5 22.6992H12.5V27.6992H37.5V22.6992Z" fill="#191919"/>
+</svg>
+            </span>
+                    </div>
+                    <div>
+                        <p class="text-gray-600 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Pellentesque
+                            sed nulla porttitor, porttitor arcu ut, dictum augue. Vestibulum consequat in urna in
+                            bibendum.
+                            Praesent sed magna risus. Nunc elementum in mauris ac pharetra. Ut blandit ut lorem sit amet
+                            rutrum. Vivamus ut purus fringilla, euismod nibh ut, blandit.
+                            Nunc ac efficitur sapien. Mauris eu lectus odio. Mauris ac erat tortor.
+                            Nulla consectetur commodo justo. Pellentesque eget ornare quam. Pellentesque sodales metus
+                            non semper luctus. Praesent non ornare tellus, eget vulputate tellus. Donec luctus non
+                            sapien
+                            sed semper.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <div
+                    class="flex flex-col justify-between bg-[#FFFFFF] p-8 w-full rounded-3xl text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
+                    <div class="flex justify-between items-center w-full">
+                        <span class="text-2xl mb-3">02. Lorem ipsum</span>
+                        <span class="h-6 w-6 flex items-center justify-center text-teal-500 ml-auto">
+               <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+<path d="M25 0C11.2154 0 0 11.2154 0 25C0 38.7846 11.2154 50 25 50C38.7846 50 50 38.7846 50 25C50 11.2154 38.7846 0 25 0ZM25 3.84615C36.7058 3.84615 46.1538 13.2942 46.1538 25C46.1538 36.7058 36.7058 46.1538 25 46.1538C13.2942 46.1538 3.84615 36.7058 3.84615 25C3.84615 13.2942 13.2942 3.84615 25 3.84615ZM23.0769 13.4615V23.0769H13.4615V26.9231H23.0769V36.5385H26.9231V26.9231H36.5385V23.0769H26.9231V13.4615H23.0769Z" fill="#191919"/>
+</svg>
+            </span>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-4">
+                <div
+                    class="flex flex-col justify-between bg-[#FFFFFF] p-8 w-full rounded-3xl text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
+                    <div class="flex justify-between items-center w-full">
+                        <span class="text-2xl mb-3">03. Lorem ipsum</span>
+                        <span class="h-6 w-6 flex items-center justify-center text-teal-500 ml-auto">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+<path d="M25 0C11.2154 0 0 11.2154 0 25C0 38.7846 11.2154 50 25 50C38.7846 50 50 38.7846 50 25C50 11.2154 38.7846 0 25 0ZM25 3.84615C36.7058 3.84615 46.1538 13.2942 46.1538 25C46.1538 36.7058 36.7058 46.1538 25 46.1538C13.2942 46.1538 3.84615 36.7058 3.84615 25C3.84615 13.2942 13.2942 3.84615 25 3.84615ZM23.0769 13.4615V23.0769H13.4615V26.9231H23.0769V36.5385H26.9231V26.9231H36.5385V23.0769H26.9231V13.4615H23.0769Z" fill="#191919"/>
+</svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-4">
+                <div
+                    class="flex flex-col justify-between bg-[#FFFFFF] p-8 w-full rounded-3xl text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
+                    <div class="flex justify-between items-center w-full">
+                        <span class="text-2xl mb-3">04. Lorem ipsum</span>
+                        <span class="h-6 w-6 flex items-center justify-center text-teal-500 ml-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+<path d="M25 0C11.2154 0 0 11.2154 0 25C0 38.7846 11.2154 50 25 50C38.7846 50 50 38.7846 50 25C50 11.2154 38.7846 0 25 0ZM25 3.84615C36.7058 3.84615 46.1538 13.2942 46.1538 25C46.1538 36.7058 36.7058 46.1538 25 46.1538C13.2942 46.1538 3.84615 36.7058 3.84615 25C3.84615 13.2942 13.2942 3.84615 25 3.84615ZM23.0769 13.4615V23.0769H13.4615V26.9231H23.0769V36.5385H26.9231V26.9231H36.5385V23.0769H26.9231V13.4615H23.0769Z" fill="#191919"/>
+</svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-4">
+                <div
+                    class="flex flex-col justify-between bg-[#FFFFFF] p-8 w-full rounded-3xl text-gray-600 font-bold cursor-pointer hover:bg-gray-300">
+                    <div class="flex justify-between items-center w-full">
+                        <span class="text-2xl mb-3">05. Lorem ipsum</span>
+                        <span class="h-6 w-6 flex items-center justify-center text-teal-500 ml-auto">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+<path d="M25 0C11.2154 0 0 11.2154 0 25C0 38.7846 11.2154 50 25 50C38.7846 50 50 38.7846 50 25C50 11.2154 38.7846 0 25 0ZM25 3.84615C36.7058 3.84615 46.1538 13.2942 46.1538 25C46.1538 36.7058 36.7058 46.1538 25 46.1538C13.2942 46.1538 3.84615 36.7058 3.84615 25C3.84615 13.2942 13.2942 3.84615 25 3.84615ZM23.0769 13.4615V23.0769H13.4615V26.9231H23.0769V36.5385H26.9231V26.9231H36.5385V23.0769H26.9231V13.4615H23.0769Z" fill="#191919"/>
+</svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('about', 'about_us');
+Route::view('/', 'welcome')->name('home');
+Route::view('dashboard','dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
